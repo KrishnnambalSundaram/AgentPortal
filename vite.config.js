ce488156f4aca1887e4b3f88eedef2ed9665f80e
Hemanth Bolla <hemanthbolla@192.168.1.4>
2025-09-25 14:37:52 +0530

    removed proxy from config

--- vite.config.js
+++ vite.config.js
@@ -5,14 +5,6 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    proxy: {
-      // when you call /api/... in frontend, it will be proxied to backend
-      '/api': {
-        target: 'http://10.182.1.24:4000',
-        changeOrigin: true,
-        secure: false,
-      },
-    },
     host: "::",
     port: 5003,
     allowedHosts: ['aiagentsportal.inflectotechnologies.com'],
